b672026b01325cf8cf68d4e37e0e96ba171b9843
Skuirrels <ty.omidi@outlook.com>
2023-12-18 23:29:44 +0000

    updated console output and add transfer of risk prop

---
+++ src/ConsoleOutput/Program.cs
@@ -0,0 +1,28 @@
+﻿using Utilities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var incoterms = GetThreeIncoterms();
+        foreach (var incoterm in incoterms)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Id: {incoterm.Id}, Name: {incoterm.Name}, Description: {incoterm.Description}, Transfer Risk: {incoterm.TransferOfRisk}" );
+            foreach (var charge in incoterm.ChargeResponsibilities)
+            {
+                Console.WriteLine($"Charge Category: {charge.Key}, Party: {charge.Value}");
+            }
+        }
+    }
+
+    static List<Incoterm> GetThreeIncoterms()
+    {
+        return new List<Incoterm>
+        {
+            Incoterms.EXW,
+            Incoterms.FCA,
+            Incoterms.FAS
+        };
+    }
+}
--- src/Utilities/Incoterms.cs
+++ src/Utilities/Incoterms.cs
@@ -26,11 +26,16 @@
         Supplier = 2
     }
 
-    public record Incoterm(int Id, string Name, string Description, Dictionary<ChargeCategory, Party> ChargeResponsibilities);
+    public record Incoterm(
+        int Id, 
+        string Name, 
+        string Description, 
+        string TransferOfRisk, 
+        Dictionary<ChargeCategory, Party> ChargeResponsibilities);
 
     public static class Incoterms
     {
-        public static readonly Incoterm EXW = new(1, "EXW", "Ex-Works", new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm EXW = new(1, "EXW", "Ex-Works", @"At Buyer's Disposal", new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -49,7 +54,7 @@
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
-        public static readonly Incoterm FCA = new(2, "FCA", "Free-Carrier",new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm FCA = new(2, "FCA", "Free-Carrier", "On Buyers Transport",new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -68,7 +73,7 @@
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
-        public static readonly Incoterm FAS = new(3, "FAS", "Free Along-Side Ship", new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm FAS = new(3, "FAS", "Free Along-Side Ship", "Alongside Ship", new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -87,7 +92,7 @@
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
-        public static readonly Incoterm FOB = new(4, "FOB", "Free On Board",new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm FOB = new(4, "FOB", "Free On Board","On Board Vessel", new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -106,7 +111,7 @@
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
-        public static readonly Incoterm CFR = new(5, "CFR", "Cost and Freight", new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm CFR = new(5, "CFR", "Cost and Freight", "On Board Vessel", new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -125,7 +130,7 @@
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
-        public static readonly Incoterm CIF = new(6, "CIF", "Cost Insurance and Freight", new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm CIF = new(6, "CIF", "Cost Insurance and Freight", "On Board Vessel", new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -144,7 +149,7 @@
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
-        public static readonly Incoterm CPT = new(7, "CPT", "Carriage Paid To",new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm CPT = new(7, "CPT", "Carriage Paid To","At Carrier",new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -163,7 +168,7 @@
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
-        public static readonly Incoterm CIP = new(8, "CIP", "Carriage Insurance Paid To", new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm CIP = new(8, "CIP", "Carriage Insurance Paid To", "At Carrier",new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -183,7 +188,7 @@
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
         
-        public static readonly Incoterm DAP = new(14, "DAP", "Delivered at Place",new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm DAP = new(14, "DAP", "Delivered at Place", "At Named Place",new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -203,7 +208,7 @@
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
         
-        public static readonly Incoterm DPU = new(12, "DPU", "Delivered at Place Unloaded",new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm DPU = new(12, "DPU", "Delivered at Place Unloaded", "At Named Place Unloaded",new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -222,7 +227,7 @@
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
-        public static readonly Incoterm DDP = new(13, "DDP", "Delivery Duty Paid", new Dictionary<ChargeCategory, Party>
+        public static readonly Incoterm DDP = new(13, "DDP", "Delivery Duty Paid", "At Named Place", new Dictionary<ChargeCategory, Party>
         {
             { ChargeCategory.ExportPackaging, Party.Supplier },
             { ChargeCategory.MarkingAndLabelling, Party.Supplier },
@@ -239,26 +244,6 @@
             { ChargeCategory.CustomsBrokerClearanceFees, Party.Supplier },
             { ChargeCategory.DeliveryToDestination, Party.Supplier },
             { ChargeCategory.DeliveryCarrierUnloading, Party.Buyer },
-            { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
-        });
-
-        public static readonly Incoterm DAT = new(15, "DAT", "Delivered at Terminal", new Dictionary<ChargeCategory, Party>
-        {
-            { ChargeCategory.ExportPackaging, Party.Supplier },
-            { ChargeCategory.MarkingAndLabelling, Party.Supplier },
-            { ChargeCategory.LoadingCharges, Party.Supplier },
-            { ChargeCategory.DeliveryToPortPlace, Party.Supplier },
-            { ChargeCategory.ExportClearance, Party.Supplier },
-            { ChargeCategory.FreightForwarderDocumentationFees, Party.Supplier },
-            { ChargeCategory.LoadingOnCarriage, Party.Supplier },
-            { ChargeCategory.OriginTerminalCharges, Party.Supplier },
-            { ChargeCategory.Freight, Party.Supplier },
-            { ChargeCategory.Insurance, Party.Supplier },
-            { ChargeCategory.DestinationTerminalCharges, Party.Supplier },
-            { ChargeCategory.ImportDutiesTaxes, Party.Buyer },
-            { ChargeCategory.CustomsBrokerClearanceFees, Party.Supplier },
-            { ChargeCategory.DeliveryToDestination, Party.Supplier },
-            { ChargeCategory.DeliveryCarrierUnloading, Party.Supplier },
             { ChargeCategory.SecurityInformationRequirements, Party.Buyer }
         });
     }
